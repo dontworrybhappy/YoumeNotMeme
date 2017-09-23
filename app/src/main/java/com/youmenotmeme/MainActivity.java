7ec91f546d021880c0732f6ee85ad7489eeb0389
osbornecharles <cosborne0731@gmail.com>
2017-09-23 08:30:01 -0400

    button

--- app/src/main/java/com/youmenotmeme/MainActivity.java
+++ app/src/main/java/com/youmenotmeme/MainActivity.java
@@ -2,6 +2,8 @@ package com.youmenotmeme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.select_photo);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                
+            }
+        });
     }
+
+
 }
