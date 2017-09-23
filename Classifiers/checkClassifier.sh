a894f74ca94f636161e2c6edf524fbd22257095a
Doug Smith <dougsmith2015@gmail.com>
2017-09-23 10:27:07 -0400

    Optimized classifier pipeline. Should generate valid data.

--- Classifiers/checkClassifier.sh
+++ Classifiers/checkClassifier.sh
@@ -1,5 +1,4 @@
 source APIKEY.sh
-classifierID="memes_1317972261"
 
 curl -X GET \
-"https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/$classifierID?api_key=$APIKEY&version=2016-05-20"
+"https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classifiers/$CLASSIFIER?api_key=$APIKEY&version=2016-05-20"
